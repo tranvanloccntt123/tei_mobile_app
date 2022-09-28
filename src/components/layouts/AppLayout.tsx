20cde3f8e69e6b7924c1f655b83a1edcab0cb7cb
teiresource <tranvanloccntt123@gmail.com>
2022-09-28 16:44:02 +0700

    update:

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { StatusBar, View, SafeAreaView } from "react-native";
+import { StatusBar, View, SafeAreaView, StyleSheet } from "react-native";
 import { AppStyle } from "../../common/AppStyle";
 import { white } from "../../common/Colors";
 import { useLoadProfile } from "../../sevices/ProfileServices";
@@ -8,13 +8,14 @@ interface PropsInterface{
     children?: ReactNode | JSX.Element | JSX.Element[],
     alterMessage?: string,
     alterID?: string | number,
-    alterStatus?: number
+    alterStatus?: number,
+    containerStyle?: any
 }
 export default function AppLayout(props: PropsInterface) {
     const message: string | undefined = props.alterMessage;
     useLoadProfile();
     const ShowAlter = React.useCallback(() => message ? <Alter status={props.alterStatus} message={message} _id={props.alterID ? props.alterID : 0} /> : <></>, [props.alterID]);
-    return <View style={[AppStyle.container, { backgroundColor: white }]}>
+    return <View style={[AppStyle.container, { backgroundColor: white }, props.containerStyle]}>
         <StatusBar translucent={true} backgroundColor={'transparent'} barStyle={'dark-content'} />
         <SafeAreaView style={[AppStyle.container]}>
             {
--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,8 +1,44 @@
 import React from "react";
-import { List } from "react-native-paper";
+import { View, StyleSheet } from "react-native";
+import { Card, List, Button, Avatar, Appbar, Text, Colors, Paragraph } from "react-native-paper";
+import { useSelector } from "react-redux";
+import { AVATAR_DEFAULT } from "../assets/images";
+import { ProfileInterface } from "../common/AppInterface";
+import { AppStyle } from "../common/AppStyle";
+import { black, gray, grayLightPrimary2, grayPrimary, red, white } from "../common/Colors";
 import AppLayout from "../components/layouts/AppLayout";
+import { COMBINE_NAME_PROFILE } from "../redux/reducers/CombineName";
+const style = StyleSheet.create({
+   cardContainer: {
+      margin: 15, 
+      padding: 15, 
+      backgroundColor: white, 
+      shadowColor: gray, 
+      shadowOffset: {
+         width: 0, 
+         height: 3
+      }, 
+      shadowOpacity: 0.5,
+      elevation: 5,
+      borderRadius: 15
+   }
+})
 export default function SettingScreen(this: any){
-     return <AppLayout>
-        <List.Item title="Profile" />
-     </AppLayout>
+   const profile: ProfileInterface = useSelector((state: any) => state[`${COMBINE_NAME_PROFILE}`].user);
+   return <AppLayout containerStyle={{backgroundColor: Colors.grey100}}>
+      <View style={style.cardContainer}>
+         <View style={{flexDirection: "row"}}>
+            <Avatar.Image source={AVATAR_DEFAULT} />
+            <View style={AppStyle.ml3}>
+               <Text style={[AppStyle.h5, {fontWeight: 'bold'}]}>{profile.name}</Text>
+               <Paragraph>{profile.email? profile.email : ""}</Paragraph>
+            </View>
+         </View>
+         <View style={[{flexDirection: "row", alignItems: "center"}, AppStyle.mt3]}>
+            <Text style={{color: gray, flex: 1}}>version 1.0.0</Text>
+            <Button style={[{alignSelf: "flex-end"},]} labelStyle={{color: black}}>Logout</Button>
+         </View>
+      </View>
+      
+   </AppLayout>
 }
